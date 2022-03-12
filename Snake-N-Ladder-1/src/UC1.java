19dee6b59d96f3d13724e1d53d388792bf42cc23
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-03-12 06:40:21 +0530

    Snake and Ladder game played with single player

---
+++ Snake-N-Ladder-1/src/UC1.java
@@ -0,0 +1,8 @@
+public class UC1{
+	public static void main(String[] args) {
+		int position=0;
+		System.out.println("Welcome To Snake And Ladder Game");
+		System.out.println("Player 1 is at position "+position);
+	}
+
+}
